6379e483674969c7a65f7155051a2bc3a562f806
CodeMeow <codemeow@yahoo.com>
2015-07-11 05:35:53 +0000

    add something

---
+++ hadoop/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+from hadoop import install
+from hadoop import start
+from hadoop import stop
---
+++ test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+import hadoop
+
+if __name__ == '__main__':
+	hadoop.stop.main()
+	hadoop.start.main()
