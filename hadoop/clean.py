5891a6caa604c71284096a80edb492e474e1cfbd
CodeMeow <codemeow@yahoo.com>
2015-07-14 03:47:13 +0000

    add something

---
+++ hadoop/clean.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import sys
+sys.path.append('..')
+
+from common import ssh
+
+def main():
+
+	hosts = ssh.init()
+
+	spark_client = ssh.filterName('spark_client', 'hostname')
+
+	ssh.cmd('sudo rm -rf /usr/local/jdk*')
+	ssh.cmd('sudo rm -rf /usr/local/hadoop*')
+	ssh.cmd('sudo rm -rf /usr/local/spark*', False, *spark_client)
+	ssh.cmd('sudo rm -rf /data')
+
+	ssh.close()
+
+if __name__ == '__main__':
+	main()
