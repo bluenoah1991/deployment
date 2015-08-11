b51f790f02c7921cb5ddd0d75789abf0794abee6
CodeMeow <codemeow@yahoo.com>
2015-08-11 10:46:07 +0000

    add something

---
+++ web/modules/hbase/hbase-install.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# parameters
+# b : backup
+# s : slaves array (comma separated)
+# z : zookeepers array (comma spearated)
+# h : hdfs path (include dir path)
+
+while getopts 'b:s:z:h:' OPT; do
+	case $OPT in
+		b)
+			b=$OPTARG;;
+		s)
+			s=$OPTARG;;
+		z)
+			z=$OPTARG;;
+		h)
+			h=$OPTARG;;
+	esac
+done
+
+if [ ! $r ] || [ ! $m ] || [ ! $s ] || [ ! $z ] || [ ! $h ]; then
+	echo 'parameters not found'
+	exit -1
+fi
+
+HBASE_URL=http://mirror.bit.edu.cn/apache/hbase/stable/hbase-1.0.1.1-bin.tar.gz
+HBASE_NAME=hbase-1.0.1.1-bin.tar.gz
+
+wget ${HBASE_URL} --output-document=/tmp/${HBASE_NAME}
+tar -zxvf /tmp/${HBASE_NAME} -C /usr/local
+
+lname=`ls -l /usr/local | grep hbase- | rev | cut -d ' ' -f1 | rev`
+ln -s /usr/local/${lname} /usr/local/hbase
+
+sed -i "/^# export JAVA_HOME=/cexport JAVA_HOME=/usr/local/jdk" /usr/local/hbase/conf/hbase-env.sh
+sed -i "/^# export HBASE_MANAGES_ZK=/cexport HBASE_MANAGES_ZK=false" /usr/local/hbase/conf/hbase-env.sh
+
+#if [[ $r == 'master' ]]; then
+#
+#	ssh-keygen -t rsa -f /tmp/id_rsa -N ''
+#	mkdir -p ~/.ssh
+#	cp /tmp/id_rsa ~/.ssh/id_rsa
+#	chmod 600 ~/.ssh/id_rsa
+#	cat /tmp/id_rsa.pub >> ~/.ssh/authorized_keys
+#	chmod 600 ~/.ssh/authorized_keys
+#	
+#
+#fi
+
+ss=(${s//,/ })
+
+echo '' > /usr/local/hbase/conf/regionservers
+for _ in ${ss[@]}
+do
+	echo ${_} >> /usr/local/hbase/conf/regionservers
+done
+
+if [[ -n ${b} ]]; then
+
+	echo ${b} > /usr/local/hbase/conf/backup-masters
+
+fi
+
+l=`grep -n '<configuration>' /usr/local/hbase/conf/hbase-site.xml | head -1 | cut -d : -f1`
+sed -i "${l}a\\
+\\t<property>\\
+\\t\\t<name>hbase.cluster.distributed</name>\\
+\\t\\t<value>true</value>\\
+\\t</property>\\
+\\t<property>\\
+\\t\\t<name>hbase.rootdir</name>\\
+\\t\\t<value>${h}</value>\\
+\\t</property>\\
+\\t<property>\\
+\\t\\t<name>hbase.zookeeper.quorum</name>\\
+\\t\\t<value>${z}</value>\\
+\\t</property>\\
+\\t<property>\\
+\\t\\t<name>hbase.zookeeper.property.clientPort</name>\\
+\\t\\t<value>2181</value>\\
+\\t</property>\\
+\\t<property>\\
+\\t\\t<name>hbase.zookeeper.property.dataDir</name>\\
+\\t\\t<value>/zookeeper</value>\\
+\\t</property>" /usr/local/hbase/conf/hbase-site.xml
+
+
---
+++ web/modules/hbase/hbase.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import sys
+sys.path.append('..')
+import common
+
+def install(cfg, ttyid):
+
+	zks = cfg.get('zks', '')
+	hdfs = cfg.get('hdfs', '')
+
+	cfg = cfg.get('hosts', None)
+	ssh = common.SSH(cfg, ttyid)
+	#maps = []
+	#for _ in cfg:
+	#	maps.append(_.get('in_ipaddr', ''))
+	#	maps.append(_.get('hostname', ''))
+	#pairs = ",".join(maps)
+	#ssh.upload(common.join(__file__, 'ipconfig-install.sh'), '/tmp/ipconfig-install.sh')
+	#ssh.cmd('chmod u+x /tmp/ipconfig-install.sh', True)
+	#for _ in cfg:
+	#	hostname = _.get('hostname', '')
+	#	ssh.cmd('/tmp/ipconfig-install.sh -h %s -s %s' % (hostname, pairs), True, False, hostname)
+
+	#jdk_local_path = common.join(__file__, "jdk-8u45-linux-x64.tar.gz")
+	#jdk_tmp_path = "/tmp/jdk-8u45-linux-x64.tar.gz"
+
+	master = ssh.haskey('hbase_master', 'hostname')
+	backup = ssh.haskey('hbase_backup', 'hostname')
+	if len(backup) == 1:
+		backup_ = backup[0]
+	else:
+		backup_ = ''
+	slaves = ssh.haskey('hbase_slave', 'hostname')
+	slave_ = ','.join(slaves)
+
+	#ssh.upload(jdk_local_path, jdk_tmp_path)
+	#ssh.cmd('tar zxvf %s -C /usr/local' % jdk_tmp_path, True)
+	ssh.upload(common.join(__file__, 'hbase-install.sh'), '/tmp/hbase-install.sh')
+	ssh.cmd('chmod u+x /tmp/hadoop-install.sh', True)
+	ssh.cmd('/tmp/hadoop-install.sh -b %s -s %s -z %s -h %s' % (backup_, slave_, zks, hdfs), True)
+
+	ssh.cmd('/usr/local/hbase/bin/start-hbase.sh', True, False, *master)
+
+	ssh.close()
+
+
